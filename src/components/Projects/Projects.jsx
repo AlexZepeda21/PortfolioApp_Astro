fe8e6cd39f580ad3ac6555b1aa2e0fff5bcb085a
AlexZepeda21 <manuelzepedagr@gmail.com>
2025-04-15 00:19:30 -0600

    Presentación inicial finalizada, algunos ajustes en diseño y escalabilidad mejorada

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,9 @@
+// src/components/MiAnimacion.jsx
+import { useState } from "react";
+
+export default function MiAnimacion() {
+
+  return (
+    <h1></h1>
+  );
+}
--- src/components/moreDown.js
+++
@@ -1,1 +0,0 @@
-const idObject = document.getElementById("moreDown")
