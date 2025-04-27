74d7ad41646f1ea29015b3858b3648514032c1d3
AlexZepeda21 <manuelzepedagr@gmail.com>
2025-04-27 11:36:54 -0600

    ajustes de diseño y section "Sobre mi"  (basico) terminado.

--- src/components/Projects/MainProject.jsx
+++ src/components/Projects/MainProject.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import FetchCategory from "./Logic/FetchCategory";
 import FetchProject from "./Logic/FetchProject";
+import "../../styles/Projects/main.css";
 
 export default function MainProject() {
   const [selectedCategory, setSelectedCategory] = useState(null);
@@ -12,13 +13,13 @@ export default function MainProject() {
   return (
     <div>
       {/* Mostrar las categorías */}
-      <div>
+      <div className="categorylist">
         <FetchCategory onSelectCategory={handleSelectCategory} />
       </div>
 
       {/* Mostrar los proyectos solo de la categoría seleccionada */}
       {selectedCategory && (
-        <div>
+        <div className="projectlist">
           <FetchProject
             id_categoria={selectedCategory.id_project_category}
             title={selectedCategory.title}
