0dae7355f59f4c865d2984a934197bf929bf1f33
AlexZepeda21 <manuelzepedagr@gmail.com>
2025-08-26 12:28:49 -0600

    Limpieza de código

---
+++ src/components/Buttons/BtnCode.jsx
@@ -0,0 +1,152 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../styles/buttons.css';
+
+export default function Btncode({ nameButton, link }) {
+    return (
+
+        <a
+            href={link}
+            aria-label="Perfil de LinkedIn"
+            className="buttonSpecial"
+        >
+            {nameButton}
+
+            <div className="star-1">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    version="1.1"
+                    style={{
+                        shapeRendering: "geometricPrecision",
+                        textRendering: "geometricPrecision",
+                        imageRendering: "optimizeQuality",
+                        fillRule: "evenodd",
+                        clipRule: "evenodd",
+                    }}
+                    viewBox="0 0 784.11 815.53"
+                >
+                    {/* paths o shapes aquí */}
+                </svg>
+                <p className="tech">
+                    Java
+                </p>
+            </div>
+            <div className="star-2">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    version="1.1"
+                    style={{
+                        shapeRendering: "geometricPrecision",
+                        textRendering: "geometricPrecision",
+                        imageRendering: "optimizeQuality",
+                        fillRule: "evenodd",
+                        clipRule: "evenodd",
+                    }}
+                    viewBox="0 0 784.11 815.53"
+                >
+                    {/* paths o shapes aquí */}
+                </svg>
+                <p className="tech">
+                    Js
+                </p>
+            </div>
+            <div className="star-3">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    version="1.1"
+                    style={{
+                        shapeRendering: "geometricPrecision",
+                        textRendering: "geometricPrecision",
+                        imageRendering: "optimizeQuality",
+                        fillRule: "evenodd",
+                        clipRule: "evenodd",
+                    }}
+                    viewBox="0 0 784.11 815.53"
+                >
+                    {/* paths o shapes aquí */}
+                </svg>
+                <p className="tech">
+                    C#
+                </p>
+            </div>
+            <div className="star-4">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    version="1.1"
+                    style={{
+                        shapeRendering: "geometricPrecision",
+                        textRendering: "geometricPrecision",
+                        imageRendering: "optimizeQuality",
+                        fillRule: "evenodd",
+                        clipRule: "evenodd",
+                    }}
+                    viewBox="0 0 784.11 815.53"
+                >
+                    {/* paths o shapes aquí */}
+                </svg>
+                <p className="tech">
+                    SQL
+                </p>
+            </div>
+            <div className="star-5">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    version="1.1"
+                    style={{
+                        shapeRendering: "geometricPrecision",
+                        textRendering: "geometricPrecision",
+                        imageRendering: "optimizeQuality",
+                        fillRule: "evenodd",
+                        clipRule: "evenodd",
+                    }}
+                    viewBox="0 0 784.11 815.53"
+                >
+                    {/* paths o shapes aquí */}
+                </svg>
+                <p className="tech">
+                    PHP
+                </p>
+            </div>
+            <div className="star-6">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    version="1.1"
+                    style={{
+                        shapeRendering: "geometricPrecision",
+                        textRendering: "geometricPrecision",
+                        imageRendering: "optimizeQuality",
+                        fillRule: "evenodd",
+                        clipRule: "evenodd",
+                    }}
+                    viewBox="0 0 784.11 815.53"
+                >
+                    {/* paths o shapes aquí */}
+                </svg>
+                <p className="tech">
+                    C++
+                </p>
+            </div>
+            <div className="star-7">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    version="1.1"
+                    style={{
+                        shapeRendering: "geometricPrecision",
+                        textRendering: "geometricPrecision",
+                        imageRendering: "optimizeQuality",
+                        fillRule: "evenodd",
+                        clipRule: "evenodd",
+                    }}
+                    viewBox="0 0 784.11 815.53"
+                >
+                    {/* paths o shapes aquí */}
+                </svg>
+                <p className="tech">
+                    C
+                </p>
+            </div>
+
+            
+
+        </a>
+    );
+}
